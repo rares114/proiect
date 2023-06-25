f953c7d4fea1f88af12495296007f0f74fd88164
Bogdan Ionescu <bogdica76@gmail.com>
2023-06-25 16:15:04 +0300

    login form antd

--- Frontend/client/src/components/Login.jsx
+++ Frontend/client/src/components/Login.jsx
@@ -3,31 +3,14 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Button, Form, Input } from "antd";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleLoginSubmit = async (e) => {
-    e.preventDefault();
-
-    const payload = {
-      email,
-      password,
-    };
-
+  const onFinish = async (values) => {
+    const response = await axios.post("/users/login", values);
     try {
-      const response = await axios.post("/users/login", payload);
-
       if (response.status === 200) {
         console.log(response.data);
         toast.success("Login successful");
@@ -51,36 +34,24 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <form onSubmit={handleLoginSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="login-input"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="login-input"
-          />
-        </div>
-        <div className="login-button-container">
-          <button type="submit">Login</button>
-        </div>
-        <div className="register-link-container">
-          <p>
-            Don't have an account? <Link to="/register">Register</Link>
-          </p>
-        </div>
-      </form>
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        onFinish={onFinish}
+      >
+        <Form.Item label="Email" name="email">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
